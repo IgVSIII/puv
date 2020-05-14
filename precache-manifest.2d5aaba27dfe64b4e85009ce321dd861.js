00514226035ace0fd1eac1b76d44d12335d44635
IgVSIII <40248988+IgVSIII@users.noreply.github.com>
2020-05-14 10:56:49 +0300

    Updates

---
+++ precache-manifest.2d5aaba27dfe64b4e85009ce321dd861.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e1977d4ea181fbb365d508937be594d",
+    "url": "/puv/index.html"
+  },
+  {
+    "revision": "3a5339d1e98470ffc2b6",
+    "url": "/puv/static/css/main.91034518.chunk.css"
+  },
+  {
+    "revision": "79b8aaf0afe381585fac",
+    "url": "/puv/static/js/2.aa48cbeb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/puv/static/js/2.aa48cbeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a5339d1e98470ffc2b6",
+    "url": "/puv/static/js/main.6a70a065.chunk.js"
+  },
+  {
+    "revision": "ba98c46b08b21b56cada",
+    "url": "/puv/static/js/runtime-main.1beb3cd6.js"
+  },
+  {
+    "revision": "3550dde5ed40b37885feeb3c336ae558",
+    "url": "/puv/static/media/startPhoto1.3550dde5.jpg"
+  }
+]);
